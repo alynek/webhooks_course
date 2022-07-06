b2a96b206108980de1f7aba7648776e4e9d59825
alynek <alyne0x67@gmail.com>
2022-07-05 21:06:46 -0300

    Create logic for create a subscription

--- AirlineApi/Controllers/WebhookSubscriptionController.cs
+++ AirlineApi/Controllers/WebhookSubscriptionController.cs
@@ -1,6 +1,9 @@
 using AirlineApi.Data;
 using AirlineApi.Dtos;
+using AirlineApi.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AirlineApi.Controllers
 {
@@ -9,15 +12,41 @@ namespace AirlineApi.Controllers
     public class WebhookSubscriptionController : ControllerBase
     {
         private readonly AirlineDbContext _context;
+        private readonly IMapper _mapper;
 
-        public WebhookSubscriptionController(AirlineDbContext context)
+        public WebhookSubscriptionController(AirlineDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
+        [HttpPost]
         public ActionResult<WebhookSubscriptionReadDto> CreateSubscription(WebhookSubscriptionCreateDto webSubscription)
         {
+            var subscription = _context.WebhookSubscriptions
+                                .AsNoTracking().
+                                FirstOrDefault(s => s.WebhookURI == webSubscription.WebhookURI);
 
+            if(subscription is null)
+            {
+                subscription = _mapper.Map<WebhookSubscription>(webSubscription);
+
+                subscription.Secret = Guid.NewGuid().ToString();
+                subscription.WebhookPublisher = "PanAus";
+                try
+                {
+                    _context.WebhookSubscriptions.Add(subscription);
+                    _context.SaveChanges();
+                }
+                catch(Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+
+                return Ok(_mapper.Map<WebhookSubscriptionReadDto>(subscription));
+            }
+
+            return NoContent();
         }
     }
 }
