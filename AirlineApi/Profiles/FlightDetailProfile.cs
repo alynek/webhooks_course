81d6b63c515c2e48ae4b05c2b953beaef7d85ace
alynek <alyne0x67@gmail.com>
2022-07-09 00:01:27 -0300

    Create FlightDetailProfile to automapper

---
+++ AirlineApi/Profiles/FlightDetailProfile.cs
@@ -0,0 +1,17 @@
+
+using AirlineApi.Dtos;
+using AirlineApi.Models;
+using AutoMapper;
+
+namespace AirlineWeb.Profiles
+{
+    public class FlightDetailProfile : Profile
+    {
+        public FlightDetailProfile()
+        {
+            CreateMap<FlightDetail, FlightDetailReadDto>();
+            CreateMap<FlightDetailCreateDto, FlightDetail>();
+            CreateMap<FlightDetailUpdateDto, FlightDetail>();
+        }
+    }
+}
