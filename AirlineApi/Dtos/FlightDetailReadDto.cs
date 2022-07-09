adc39013dc11bade7c4e3c960dfc0b807976dbad
alynek <alyne0x67@gmail.com>
2022-07-08 23:24:18 -0300

    Create dto's to FlightDetail class

---
+++ AirlineApi/Dtos/FlightDetailReadDto.cs
@@ -0,0 +1,11 @@
+namespace AirlineApi.Dtos
+{
+    public class FlightDetailReadDto
+    {
+        public int Id { get; set; }
+
+        public string FlightCode { get; set; }
+        
+        public decimal Price { get; set; }
+    }
+}
---
+++ AirlineApi/Dtos/FlightDetailUpdateDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+namespace AirlineApi.Dtos
+{
+    public class FlightDetailUpdateDto
+    {
+        [Required]
+        public string FlightCode { get; set; }
+        
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
