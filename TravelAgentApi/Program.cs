036bb5e24f0f40c048ecea75cb7e1df7bc098441
alynek <alyne0x67@gmail.com>
2022-07-13 22:18:10 -0300

    Configure dbcontext on startup

--- TravelAgentApi/Program.cs
+++ TravelAgentApi/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using TravelAgentApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<TravelAgentDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TravelAgentConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
