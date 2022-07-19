bcb4959d07ff72921fe947b07b188e7b038648f7
alynek <alyne0x67@gmail.com>
2022-07-19 19:14:00 -0300

    Configure project

--- AirlineSendAgent/Program.cs
+++ AirlineSendAgent/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AirlineSendAgent.App;
+using AirlineSendAgent.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) => {
+                    services.AddSingleton<IAppHost, AppHost>();
+                    services.AddDbContext<SendAgentDbContext>(options => 
+                        options.UseSqlServer(context.Configuration.GetConnectionString("AirLineConnection")));
+                    services.AddHttpClient();
+                }).Build();
+
+host.Services.GetService<IAppHost>().Run();
